98c15785b939b1ec48fea442dd16ee47464ca9e4
Daniel Lagos <daniel.lagos010@gmail.com>
2021-08-10 21:28:27 -0500

    add new console appearance

--- index.js
+++ index.js
@@ -22,21 +22,22 @@ const main = async () => {
 
     switch (opt) {
       case 1:
-        //TODO: Show message
         const textSearch = await readInput('City: ');
         const places = await searches.city(textSearch);
         const idSelected = await listPlaces(places);
         const placeSelected = places.find((place) => place.id === idSelected);
         const weatherPlaceSelected = await searches.placeWeather(
           placeSelected.lat, placeSelected.lng);
+
+        console.clear();
         console.log('\n Place Information \n'.green);
-        console.log('City:', placeSelected.name);
+        console.log('City:', placeSelected.name.green);
         console.log('Length:', placeSelected.lng);
         console.log('Latitude:', placeSelected.lat);
         console.log('Temperature:', weatherPlaceSelected.temp);
         console.log('Minimum:', weatherPlaceSelected.min);
         console.log('Maximum:', weatherPlaceSelected.max);
-        console.log('Weather:', weatherPlaceSelected.description);
+        console.log('Weather:', weatherPlaceSelected.description.green);
 
         break;
       case 2:
